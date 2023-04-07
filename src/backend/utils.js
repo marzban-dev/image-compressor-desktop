30a7a289390f9972323fade39a351e00c3d01d18
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-04-07 23:55:37 +0430

    Update utils file

--- src/backend/utils.js
+++ src/backend/utils.js
@@ -1,8 +1,8 @@
-const {promises: fsPromises} = require("fs");
+const { promises: fsPromises, existsSync } = require("fs");
 const path = require("path");
 const sharp = require("sharp");
-const {sleep} = require("../utils");
-const {formatBytes} = require("../utils");
+const { sleep } = require("../utils");
+const { formatBytes } = require("../utils");
 
 exports.getDirectoryFilesCount = async (e, directorPath) => {
     const files = await fsPromises.readdir(directorPath);
@@ -14,7 +14,6 @@ exports.getDirectoryFiles = async (e, directorPath) => {
 };
 
 exports.compressor = async (input, output, quality = 40, callback) => {
-
     let index = 1;
     let images = await this.getDirectoryFiles(null, input);
     let analyses = [];
@@ -29,24 +28,32 @@ exports.compressor = async (input, output, quality = 40, callback) => {
             imageSizes.push(imageSize);
         }
 
-        return imageSizes.reduce((oldValue, imageSize) => imageSize > oldValue ? imageSize : oldValue, 0);
-    }
+        return imageSizes.reduce(
+            (oldValue, imageSize) => (imageSize > oldValue ? imageSize : oldValue),
+            0
+        );
+    };
 
     for (let image of images) {
         const fileExt = path.extname(image);
         const inputPath = path.join(input, image);
-        const outputPath = path.join(output, image);
 
+        const outputDefault = path.join(input, "..", "/output");
+        const outputPath = output ? path.join(output, image) : path.join(outputDefault, image);
+
+        if (!output && !existsSync(outputDefault)) {
+            await fsPromises.mkdir(outputDefault);
+        }
 
         try {
             let sharpObj = sharp(inputPath);
 
             switch (fileExt) {
-                case ".jpg" :
-                    sharpObj = sharpObj.jpeg({quality, mozjpeg: true});
+                case ".jpg":
+                    sharpObj = sharpObj.jpeg({ quality, mozjpeg: true });
                     break;
-                case ".png" :
-                    sharpObj = sharpObj.png({quality});
+                case ".png":
+                    sharpObj = sharpObj.png({ quality });
                     break;
             }
 
@@ -59,7 +66,7 @@ exports.compressor = async (input, output, quality = 40, callback) => {
                 id: index,
                 fileName: image,
                 oldSize,
-                newSize
+                newSize,
             });
 
             const biggestSize = await calculateBiggestSize();
@@ -70,23 +77,23 @@ exports.compressor = async (input, output, quality = 40, callback) => {
                 newSize,
                 progress: [images.length, index],
                 analyses,
-                biggestSize
+                biggestSize,
             });
 
             index++;
 
             await sleep(10);
         } catch (e) {
-            console.log("ERROR");
+            console.log("ERROR", e);
         }
     }
 
-    const inputSize = formatBytes(analyses.reduce((total, {oldSize}) => total + oldSize, 0));
-    const outputSize = formatBytes(analyses.reduce((total, {newSize}) => total + newSize, 0));
+    const inputSize = formatBytes(analyses.reduce((total, { oldSize }) => total + oldSize, 0));
+    const outputSize = formatBytes(analyses.reduce((total, { newSize }) => total + newSize, 0));
 
     return {
         inputSize,
         outputSize,
-        difference: inputSize - outputSize
-    }
+        difference: inputSize - outputSize,
+    };
 };
